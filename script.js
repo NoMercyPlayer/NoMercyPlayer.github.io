bb1f3c29da01b5cdb5e8e382daf882a51a585855
NoMercyPlayer <74675136+NoMercyPlayer@users.noreply.github.com>
2024-03-09 15:32:47 +0000

    New Idea
    New Idea!

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var buttons = document.querySelectorAll('button[id^="downloadBtn"]');
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            var a = document.createElement('a');
+            // Replace these example links with your actual file links
+            switch (button.id) {
+                case 'downloadBtn1':
+                    a.href = 'https://drive.usercontent.google.com/download?id=1vDGFi2ODGYS8iIHno2rSfCpd7QeDLNqL&export=download&authuser=0';
+                    break;
+                case 'downloadBtn2':
+                    a.href = 'https://drive.usercontent.google.com/download?id=1N3kK-dWDAE0sG67KPMrbCYany5Wg9REh&export=download&authuser=0';
+                    break;
+                case 'downloadBtn3':
+                    a.href = 'https://example.com/download3';
+                    break;
+            }
+            a.setAttribute('target', '_blank');
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        });
+    });
+});
